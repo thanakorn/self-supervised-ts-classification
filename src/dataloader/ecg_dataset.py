495447d583f25ebbd4b090c4068cccaa86d628d1
Thanakorn <ThanakornPany@pttep.com>
2021-11-23 22:49:44 +0700

    Add ECGDataset

---
+++ src/dataloader/ecg_dataset.py
@@ -0,0 +1,15 @@
+from torch.utils.data.dataset import Dataset
+
+class ECGDataset(Dataset):
+    def __init__(self, data, labels = None) -> None:
+        self.data = data
+        self.labels = labels
+
+    def __getitem__(self, index):
+        x = self.data[index]
+        
+        if self.labels is not None:
+            y = self.labels[index]
+            return x, y
+        else:
+            return x
